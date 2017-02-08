3a742961aab08f269b01bcac652c02e7958bb59c
Charlie Harrington <charlie@whatrocks.org>
2017-02-07 20:56:59 -0800

    set up app with typescript

---
+++ src/index.tsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+interface Props {
+
+}
+
+interface State {
+
+}
+
+export default class App extends Component<Props, State> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>
+          Welcome to Von Count!!!!
+        </Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  } as React.ViewStyle,
+
+  text: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  } as React.TextStyle
+});
