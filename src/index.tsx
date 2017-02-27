c0ef90686b3962dc7beb8f939d5154869cf409cd
Charlie Harrington <charlie@whatrocks.org>
2017-02-26 16:59:56 -0800

    moving ball around with Animated.ValueXY

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,8 @@ import {
   ListView,
   TouchableHighlight,
   Animated,
-  Easing
+  Easing,
+  Dimensions
 } from 'react-native';
 
 interface Props {
@@ -15,8 +16,13 @@ interface Props {
 
 interface State {
   count?: number,
-  animatedValue?: any
+  increment?: number,
+  pan?: any
 }
+
+const { width, height } = Dimensions.get("window");
+const vonCountSize = 100;
+
 
 export default class App extends Component<Props, State> {
 
@@ -24,27 +30,22 @@ export default class App extends Component<Props, State> {
     super(props);
     this.state = {
       count: 0,
-      animatedValue: new Animated.Value(100)
+      increment: 1,
+      pan: new Animated.ValueXY()
     };
   }
 
-  componentDidMount() {
-    Animated.timing(this.state.animatedValue, {
-      toValue: 200,
-      duration: 1000,
-      easing: Easing.bounce
-    }).start();
-  }
-
   render() {
-    const animatedStyle = { height: this.state.animatedValue };
+    const animatedStyle = { transform: this.state.pan.getTranslateTransform() };
     return (
       <View style={styles.container}>
-        <Animated.View style={[styles.box, animatedStyle]}/>
+        <View style={styles.row}>
+          <Animated.View style={[styles.vonCount, animatedStyle]}></Animated.View>
+        </View>
         <Text style={styles.counter}>
           {this.state.count}
         </Text>
-        <View style={styles.row}>
+        <View style={styles.buttonRow}>
           <TouchableHighlight 
             style={styles.button}
             onPress={this.decrement.bind(this)}
@@ -65,15 +66,25 @@ export default class App extends Component<Props, State> {
   increment(e) {
     e.preventDefault();
     this.setState({
-      count: this.state.count + 1
-    })
+      count: this.state.count + this.state.increment,
+    });
+    Animated.spring(this.state.pan, {
+        tension: 2,
+        friction: 3, 
+        toValue: {x: this.state.count + 50, y: 0}
+    }).start();
   }
 
   decrement(e) {
     e.preventDefault();
     this.setState({
-      count: this.state.count - 1
-    })
+      count: this.state.count - this.state.increment
+    });
+    Animated.spring(this.state.pan, {
+        tension: 2,
+        friction: 3, 
+        toValue: {x: this.state.count - 50, y: 0}
+    }).start();
   }
 
 
@@ -82,8 +93,13 @@ export default class App extends Component<Props, State> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#F5FCFF'
+    justifyContent: 'flex-end',
+    backgroundColor: '#EFEFEF'
+  } as React.ViewStyle,
+
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'center'
   } as React.ViewStyle,
 
   counter: {
@@ -92,28 +108,39 @@ const styles = StyleSheet.create({
     margin: 10
   } as React.TextStyle,
 
+  vonCount: {
+    height: vonCountSize,
+    width: vonCountSize,
+    borderRadius: vonCountSize / 2,
+    backgroundColor: '#7f7fff',
+  } as React.ViewStyle,
+
   button: {
     justifyContent: 'center',
     alignItems: 'center',
-    width: 50,
+    width: 100,
     height: 50,
-    backgroundColor: '#7f7fff'
+    backgroundColor: '#00d3a3',
+    marginBottom: 30,
+    borderRadius: 3
   } as React.ViewStyle,
 
   buttonText: {
-    fontSize: 30,
+    fontSize: 40,
+    color: '#efefef',
+    fontWeight: 'bold',
     textAlign: 'center'
   } as React.TextStyle,
 
-  row: {
+  buttonRow: {
     flexDirection: 'row',
     justifyContent: 'space-around',
   } as React.ViewStyle,
 
   box: {
     backgroundColor: '#333',
-    width: 100,
-    height: 100
+    width: 50,
+    height: 50
   } as React.ViewStyle
 
 });
