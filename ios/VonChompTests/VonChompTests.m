7c8c227556e9f20488d35bf24c5b7590fad44d8f
Charlie Harrington <charlie@whatrocks.org>
2017-09-14 21:19:19 -0700

    more rename magic

--- ios/VonChompTests/VonChompTests.m
+++ ios/VonChompTests/VonChompTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface VonCountTests : XCTestCase
+@interface VonChompTests : XCTestCase
 
 @end
 
-@implementation VonCountTests
+@implementation VonChompTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
